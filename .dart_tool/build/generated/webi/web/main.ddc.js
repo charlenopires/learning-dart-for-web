3d7e59d7b089c41fd5ca4ccefe3eca6dbd9a1405
Charleno Pires <charleno@gmail.com>
2018-04-28 21:17:57 -0300

    Chaging for english lang how primary on code and finished about counting vowels and consonants

--- .dart_tool/build/generated/webi/web/main.ddc.js
+++ .dart_tool/build/generated/webi/web/main.ddc.js
@@ -9,45 +9,67 @@ define(['dart_sdk'], function(dart_sdk) {
   const $join = dartx.join;
   const $reversed = dartx.reversed;
   const $split = dartx.split;
-  const $text = dartx.text;
+  const $length = dartx.length;
+  const $innerHtml = dartx.innerHtml;
+  const $toString = dartx.toString;
   const $onClick = dartx.onClick;
   let EventToNull = () => (EventToNull = dart.constFn(dart.fnType(core.Null, [html.Event])))();
-  main.Palavra = class Palavra extends core.Object {
+  main.Word = class Word extends core.Object {
     get desc() {
       return this[desc$];
     }
     set desc(value) {
-      this[desc$] = value;
+      super.desc = value;
     }
-    reverso() {
+    reverse() {
       return this.desc[$split]('')[$reversed][$join]();
     }
+    countVowels() {
+      let r = core.RegExp.new("[aeiouAEIOU]");
+      return r.allMatches(this.desc)[$length];
+    }
+    countConsonants() {
+      let r = core.RegExp.new("[^aeiouAEIOU]");
+      core.print(r.allMatches(this.desc));
+      return r.allMatches(this.desc)[$length];
+    }
+    get showDesc() {
+      return this.desc;
+    }
   };
-  (main.Palavra.new = function(desc) {
+  (main.Word.new = function(desc) {
     this[desc$] = desc;
-  }).prototype = main.Palavra.prototype;
-  dart.addTypeTests(main.Palavra);
-  const desc$ = Symbol("Palavra.desc");
-  dart.setMethodSignature(main.Palavra, () => ({
-    __proto__: dart.getMethods(main.Palavra.__proto__),
-    reverso: dart.fnType(core.String, [])
+  }).prototype = main.Word.prototype;
+  dart.addTypeTests(main.Word);
+  const desc$ = Symbol("Word.desc");
+  dart.setMethodSignature(main.Word, () => ({
+    __proto__: dart.getMethods(main.Word.__proto__),
+    reverse: dart.fnType(core.String, []),
+    countVowels: dart.fnType(core.int, []),
+    countConsonants: dart.fnType(core.int, [])
   }));
-  dart.setFieldSignature(main.Palavra, () => ({
-    __proto__: dart.getFields(main.Palavra.__proto__),
-    desc: dart.fieldType(core.String)
+  dart.setGetterSignature(main.Word, () => ({
+    __proto__: dart.getGetters(main.Word.__proto__),
+    showDesc: core.String
+  }));
+  dart.setFieldSignature(main.Word, () => ({
+    __proto__: dart.getFields(main.Word.__proto__),
+    desc: dart.finalFieldType(core.String)
   }));
   main.main = function() {
-    let btn = html.ButtonElement._check(html.querySelector("#btn"));
+    let btn = html.ButtonElement._check(html.querySelector("#btn_reverse"));
     btn[$onClick].listen(dart.fn(e => {
       let msg = html.querySelector("#msg");
-      let p = html.InputElement._check(html.querySelector("#palavra"));
-      let palavra = new main.Palavra.new(p.value);
-      msg[$text] = palavra.reverso();
+      let p = html.InputElement._check(html.querySelector("#word"));
+      let word = new main.Word.new(p.value);
+      msg[$innerHtml] = dart.toString(word.countVowels()) + " vowels";
+      msg[$innerHtml] = dart.notNull(msg[$innerHtml]) + "<br>";
+      msg[$innerHtml] = dart.notNull(msg[$innerHtml]) + (dart.toString(word.countConsonants()) + " consonants");
     }, EventToNull()));
   };
   dart.trackLibraries("web/main.ddc", {
     "main.dart": main
-  }, '{"version":3,"sourceRoot":"","sources":["main.dart"],"names":[],"mappings":";;;;;;;;;;;;;;;IAGS;;;;;;;AAIN,YAAO,UAAS,QAAM,CAAC,cAAY,OAAK;IACzC;;+BAJQ,IAAS;IAAJ,WAAI,GAAJ,IAAI;EAAC;;;;;;;;;;;;AAQlB,QAAc,gCAAM,kBAAa,CAAC;AAClC,OAAG,UAAQ,OAAO,CAAC,QAAC,CAAO;AACzB,UAAQ,MAAM,kBAAa,CAAC;AAC5B,UAAa,6BAAI,kBAAa,CAAC;AAC/B,UAAQ,cAAU,gBAAO,CAAC,CAAC,MAAM;AACjC,SAAG,OAAK,GAAG,OAAO,QAAQ;;EAE9B","file":"main.ddc.js"}');
+  }, '{"version":3,"sourceRoot":"","sources":["main.dart"],"names":[],"mappings":";;;;;;;;;;;;;;;;;IAGe;;;;;;;AAIZ,YAAO,UAAS,QAAM,CAAC,cAAY,OAAK;IACzC;;AAGE,UAAO,IAAI,AAAI,eAAM,CAAC;AACtB,YAAO,EAAC,WAAW,CAAC,SAAS,UAAQ;IACvC;;AAGE,UAAO,IAAI,AAAI,eAAM,CAAC;AACtB,gBAAK,CAAC,CAAC,WAAW,CAAC,SAAS;AAC5B,YAAO,EAAC,WAAW,CAAC,SAAS,UAAQ;IACvC;;YAEuB,UAAS;;;4BAjB3B,IAAS;IAAJ,WAAI,GAAJ,IAAI;EAAC;;;;;;;;;;;;;;;;;;AAqBf,QAAc,gCAAM,kBAAa,CAAC;AAClC,OAAG,UAAQ,OAAO,CAAC,QAAC,CAAO;AACzB,UAAQ,MAAM,kBAAa,CAAC;AAC5B,UAAa,6BAAI,kBAAa,CAAC;AAC/B,UAAK,WAAO,aAAI,CAAC,CAAC,MAAM;AACxB,SAAG,YAAU,GAAiC,cAA9B,IAAI,YAAY,MAAgB;AAChD,SAAG,YAAU,GA/BjB,aA+BI,GAAG,YAAU,IAAI;AACjB,SAAG,YAAU,GAhCjB,aAgCI,GAAG,YAAU,KAAsC,cAAlC,IAAI,gBAAgB,MAAgB;;EAEzD","file":"main.ddc.js"}');
   // Exports:
   return {
     main: main
--- web/main.dart
+++ web/main.dart
@@ -1,20 +1,35 @@
 import 'dart:html';
 
-class Palavra {
-  String desc;
-  Palavra(this.desc);
+class Word {
+  final String desc;
+  Word(this.desc);
 
-  String reverso(){
+  String reverse(){
    return this.desc.split('').reversed.join();
   }
+
+  int countVowels(){
+    RegExp r = new RegExp("[aeiouAEIOU]");
+    return r.allMatches(this.desc).length;
+  }
+
+  int countConsonants() {
+    RegExp r = new RegExp("[^aeiouAEIOU]");
+    print(r.allMatches(this.desc));
+    return r.allMatches(this.desc).length;
+  }
+
+  String get showDesc => this.desc;
 }
 
 void main() {
   ButtonElement btn = querySelector("#btn");
   btn.onClick.listen((Event e) {
     Element msg = querySelector("#msg");
-    InputElement p = querySelector("#palavra");
-    Palavra palavra = Palavra(p.value);
-    msg.text = palavra.reverso();
+    InputElement p = querySelector("#word");
+    Word word = Word(p.value);
+    msg.innerHtml = word.countVowels().toString() + " vowels";
+    msg.innerHtml += "<br>";
+    msg.innerHtml += word.countConsonants().toString() + " consonants";
   });
 }
